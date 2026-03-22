9c6849372f46e178efcc26de9d8d89360b321321
TestPow <testpow8@gmail.com>
2026-03-22 13:23:37 +0000

    Added POST request logic

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    roll_no: int
 
 
 @app.get("/")
@@ -12,3 +19,12 @@ def read_root():
 @app.get("/greet")
 def greet_name(name: str, age: Optional[int]=None):
     return {"msg": f"Hello, {name} and your age is {age}!!"}
+
+
+@app.post("/student", status_code=201)
+def create_student(student: Student):
+    return {
+        "name": student.name,
+        "age": student.age,
+        "roll_no": student.roll_no
+    }
